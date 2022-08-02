0d613ec3bf109a59ba8105363b467c52ab72eae4
Charlene Parker <110270524+cparker1324@users.noreply.github.com>
2022-08-01 20:00:44 -0400

    Update DemoTest.java
    changed to add ()

--- src/demo/DemoTest.java
+++ src/demo/DemoTest.java
@@ -8,8 +8,8 @@ public class DemoTest {
 	}
 	
 	//For US456
-	public static void divide(int a, int b) {
-		System.out.println(a/b);
+	public static void add(int a, int b) {
+		System.out.println(a + b);
 	}
 
 }
